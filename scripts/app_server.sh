fbc5268b4ae966b53a8a7ece48d2c8ace0700513
JovanyAldair <azevedojovani90@gmail.com>
2024-07-23 18:21:32 +0100

    scripts changed

--- scripts/app_server.sh
+++ scripts/app_server.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# change to root user
+sudo su
+
+# add the following lines to /etc/hosts file
+echo "127.0.0.1       localhost    app-server.local" >> /etc/hosts
+
+echo "192.168.57.52   monitor-server.local" >> /etc/hosts
+
+# exit to root user
+exit
+
 # update and upgrade packages 
 sudo apt-get update && sudo apt-get upgrade -y
-
-# install the following packages
-sudo apt-get install -y ansible git
--- scripts/monitor_server.sh
+++ scripts/monitor_server.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# change to root user
+sudo su
+
+# add the following lines to /etc/hosts file
+echo "127.0.0.1       localhost    monitor-server.local" >> /etc/hosts
+
+echo "192.168.57.52   app-server.local" >> /etc/hosts
+
+# exit to root user
+exit
+
 sudo apt-get update && sudo apt-get upgrade -y
 
 # install python  2 and 3
